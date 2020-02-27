fbf9370514fcb10f539123556d20dc2c755e32a2
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2020-02-27 15:27:17 +0200

    second

--- src/index.js
+++ src/index.js
@@ -1,63 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const Header = (props) => {
+const Button = ({ handleClick, text }) => {
     return (
-        <h1>{props.course.name}</h1>
-    )
-}
-const Part = (props) => {
-    const part = props.part
-    return (
-        <p> {part.name} {part.exercises} </p>
+        <button onClick={handleClick}>{text}</button>
     )
 }
 
-const Content = (props)=> {
-    const parts = props.course.parts
+const Statistic = ({ text, value }) => {
     return (
         <>
-        <Part part={parts[0]} />
-        <Part part={parts[1]} />
-        <Part part={parts[2]} />
+            <tr>
+                <td>{text}</td>
+                <td>{value}</td>
+            </tr>
         </>
     )
 }
 
-const Total = (props) => {
-    const parts = props.course.parts
+const Statistics = ({ hyva, neutraali, huono }) => {
+    const yhteensa = hyva + neutraali + huono
+    const keskiarvo = (hyva + (-1 * huono)) / yhteensa
+    const positiivisia = hyva / yhteensa
+    if (yhteensa <= 0)
+        return (<div><p>Ei yhtään palautetta annettu</p></div>)
+    else
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <Statistic text="Hyvä" value={hyva} />
+                        <Statistic text="Neutraali" value={neutraali} />
+                        <Statistic text="Huono" value={huono} />
+                        <Statistic text="Yhteensä" value={yhteensa} />
+                        <Statistic text="Keskiarvo" value={keskiarvo} />
+                        <Statistic text="Positiivisia" value={positiivisia} />
+                    </tbody>
+                </table>
+            </div>
+        )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
     return (
-        <p>Yhteensä {parts[0].exercises + parts[1].exercises + parts[2].exercises} tehtävää </p>
+        <div>
+            <h1>Anna palautetta</h1>
+            <Button handleClick={() => setGood(good + 1)} text="hyvä" />
+            <Button handleClick={() => setNeutral(neutral + 1)} text="neutraali" />
+            <Button handleClick={() => setBad(bad + 1)} text="huono" />
+            <h1>Statistiikka</h1>
+            <Statistics hyva={good} neutraali={neutral} huono={bad} />
+        </div>
     )
 }
-const App = () => {
-  const course = {
-      name: 'Half stack sovelluskehitys',
-      parts: [
-          {
-          name: 'Reactin perusteet',
-          exercises: 10
-        }, 
-        {
-            name: 'Teidon välitys propseilla',
-            exercises: 7
-        },
-        {
-            name: 'Komponenttien tila',
-            exercises: 14
-        }
-      ]
-  }
 
-
-  return (
-    <div>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
